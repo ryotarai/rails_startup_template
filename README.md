567755dfb736e70672f4b9484f84b7805c9b655a
Ryota Arai <ryota.arai@gmail.com>
2013-08-31 11:42:28 +0900

    Make bootstrap install selectable.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Rails Startup Template
 ======================
 ```
-rails new [app_name] -m rails_startup_template/rails4_template.rb
+rails new [app_name] -m rails_startup_template/rails4_template.rb -T
 ```
--- rails4_template.rb
+++ rails4_template.rb
@@ -1,12 +1,12 @@
-if yes?("Use slim instead of erb?")
-  gem "slim-rails"
-elsif yes?("Use haml instead of erb?")
-  gem "haml-rails"
+if yes?("Use another template engine instead of erb?")
+  template_engine = ask('What name is the template engine? (e.g. slim or haml)')
+  gem "#{template_engine}-rails"
 end
+use_bootstrap = yes?('Install twitter bootstrap?')
 
 gem "therubyracer"
 gem "less-rails"
-gem "twitter-bootstrap-rails"
+gem "twitter-bootstrap-rails" if use_bootstrap
 
 # Simple form builder (https://github.com/plataformatec/simple_form)
 gem "simple_form"
@@ -42,23 +42,24 @@ run "bundle exec guard init spork"
 run "bundle exec guard init rspec"
 
 # Install bootstrap
-run "bundle exec rails generate bootstrap:install less"
+if use_bootstrap
+  run "bundle exec rails generate bootstrap:install less"
 
-if yes?("Use fixed bootstrap layout?")
-  run "bundle exec rails g bootstrap:layout -f application fixed"
-elsif yes?("Use fluid bootstrap layout?")
-  run "bundle exec rails g bootstrap:layout -f application fluid"
+  if yes?("Use fixed bootstrap layout?")
+    run "bundle exec rails g bootstrap:layout -f application fixed"
+  elsif yes?("Use fluid bootstrap layout?")
+    run "bundle exec rails g bootstrap:layout -f application fluid"
+  end
+
+  remove_file "app/views/layouts/application.html.erb"
+  remove_file "app/assets/stylesheets/scaffolds.css.scss"
 end
-
-remove_file "app/views/layouts/application.html.erb"
-remove_file "app/assets/stylesheets/scaffolds.css.scss"
 
 
 # Install simple_form
 run "bundle exec rails generate simple_form:install --bootstrap"
 
 run "echo 'body { padding-top: 60px; }' >> app/assets/stylesheets/bootstrap_and_overrides.css.less"
-
 
 puts "TODO: Edit spec_helper.rb"
 
